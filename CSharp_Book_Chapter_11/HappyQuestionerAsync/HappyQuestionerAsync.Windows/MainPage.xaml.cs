60308e4346adb39ba72fbf435d6ea807f402c449
OriolG <download.spam.net@gmail.com>
2016-09-12 11:12:17 +0200

    created areyouhappy? project

---
+++ CSharp_Book_Chapter_11/HappyQuestionerAsync/HappyQuestionerAsync.Windows/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HappyQuestionerAsync
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        private int i = 0;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            response.Text = string.Empty;
+            timer.Tick += timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(50);
+            timer.Start();
+            CheckHappines();
+        }
+
+        private async void CheckHappines()
+        {
+            MessageDialog dialog = new MessageDialog("Are you happy?");
+            dialog.Commands.Add(new UICommand("Happy as a clam!"));
+            dialog.Commands.Add(new UICommand("Sad as a donkey."));
+            dialog.DefaultCommandIndex = 1;
+            UICommand result = await dialog.ShowAsync() as UICommand;
+            if(result != null && result.Label == "Happy as a clam!")
+            {
+                response.Text = "The user is happy";
+            }
+            else
+            {
+                response.Text = "The user is sad";
+            }
+            timer.Stop();
+        }
+
+        void timer_Tick(object sender, object e)
+        {
+            ticker.Text = $"Tick #{i++}";
+        }
+    }
+}
