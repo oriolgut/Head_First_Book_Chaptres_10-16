1611c3170f640d82eed2e983576fb5f799e3d32a
OriolG <download.spam.net@gmail.com>
2016-09-26 14:54:56 +0200

    Createt and finished Project "Robust Guy"

---
+++ CSharp_Book_Chapter_13/RobustGuy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RobustGuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.CreateGuy();
+        }
+
+        public void CreateGuy()
+        {
+            Console.Write("Enter birthday: ");
+            string birthday = Console.ReadLine();
+            Console.Write("Enter height in cm: ");
+            string height = Console.ReadLine();
+            RobustGuy guy = new RobustGuy(birthday, height);
+            Console.WriteLine(guy.ToString());
+            Console.Write("want to restart? (y/n): ");
+            string answer = Console.ReadLine();
+            if (answer.Equals("y"))
+            {
+                CreateGuy();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
