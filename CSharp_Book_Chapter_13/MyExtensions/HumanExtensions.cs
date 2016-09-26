e440a598bcd6f5896d3a085085d3eead8d4262a8
OriolG <download.spam.net@gmail.com>
2016-09-26 15:03:03 +0200

    Create and finished Project "MyExtensions"

---
+++ CSharp_Book_Chapter_13/MyExtensions/HumanExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtension
+{
+    public static class HumanExtensions
+    {
+        public static bool IsDistressCall(this string s)
+        {
+            if (s.Contains("Help!"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CSharp_Book_Chapter_13/MyExtensions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyExtension;
+
+namespace MyExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Clones are wreaking havoc at the factory. Help!";
+            Console.WriteLine(message.IsDistressCall());
+            Console.ReadKey();
+        }
+    }
+}
