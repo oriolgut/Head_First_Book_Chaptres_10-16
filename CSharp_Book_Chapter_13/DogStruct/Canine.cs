61b0394c347af034ab1dc0bd7319c2e09a798d4e
OriolG <download.spam.net@gmail.com>
2016-09-26 14:01:52 +0200

    Create and finished "struct Dog" Project

---
+++ CSharp_Book_Chapter_13/DogStruct/Canine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogStruct
+{
+    public class Canine
+    {
+        public string Name;
+        public string Breed;
+
+        public Canine(string name, string breed)
+        {
+            Name = name;
+            Breed = breed;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine($"My Name is {Name} an I'm a {Breed}.");
+        }
+    }
+}
---
+++ CSharp_Book_Chapter_13/DogStruct/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogStruct
+{
+    public struct Dog
+    {
+        public string Name;
+        public string Breed;
+
+        public Dog(string name, string breed)
+        {
+            Name = name;
+            Breed = breed;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine($"My Name is {Name} an I'm a {Breed}.");
+        }
+    }
+}
