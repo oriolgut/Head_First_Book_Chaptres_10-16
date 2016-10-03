c9e75fc876b8ce5b936b5f7fe40422d6a598ad33
OriolG <download.spam.net@gmail.com>
2016-10-03 11:38:27 +0200

    Create Solution and add Project BaseBall Simulator

---
+++ CSharp_Book_Chapter_14/JimmyComicsSplitApp/DataModel/Purchase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JimmyComicsSplitApp.DataModel
+{
+    class Purchase
+    {
+        public int Issue { get; set; }
+        public decimal Price { get; set; }
+    }
+}
