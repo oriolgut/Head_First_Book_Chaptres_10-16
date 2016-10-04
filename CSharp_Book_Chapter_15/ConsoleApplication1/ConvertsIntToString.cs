34cdd34472d766594487106298eba9a0b5bdf2d0
OriolG <download.spam.net@gmail.com>
2016-10-04 08:42:59 +0200

    Created and finished Project for Excercise the theme Delegates

---
+++ CSharp_Book_Chapter_15/ConsoleApplication1/ConvertsIntToString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ConvertsIntToString
+    {
+        delegate string ConvertIntToString(int i);
+
+        private static string HiThere(int i)
+        {
+            return $"Hi there! #{i * 100}";
+        }
+
+        static void Main(string[] args)
+        {
+            ConvertIntToString someMethod = new ConvertIntToString(HiThere);
+            string message = someMethod(5);
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
